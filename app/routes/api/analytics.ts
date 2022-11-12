00d64d6d61d29a6c77d929a70898392ede44e376
Gaurav Singh <ckzo@proton.me>
2022-11-12 21:41:21 +0530

    chore: move remix to app server

--- app/routes/api/analytics.ts
+++ app/routes/api/analytics.ts
@@ -1,18 +1,53 @@
 import { json } from "@remix-run/node";
 import type { LoaderArgs } from "@remix-run/node";
+import { db } from "~/utils/db.server";
+import parser from "ua-parser-js";
+// import isbot from 'isbot'
+// import geoip from 'geoip-country'
+// Nodejs HTTP
+// isbot(request.getHeader('User-Agent'))
+
+// var ip = "207.97.227.239";
+// var geo = geoip.lookup(ip);
+
+// console.log(geo);
+// { range: [ 3479299040, 3479299071 ],
+//   country: 'US'}
+
 export async function loader({ request }: LoaderArgs) {
+  const ua = request.headers.get("user-agent");
+  const usera = parser(ua);
+  const device = `${usera.browser.name} ${usera.browser.major}, ${usera.os.name}`;
   console.log(request.method);
   const requestUrl = new URL(request.url);
   //   console.log(url);
   const term = requestUrl.searchParams;
   console.log(term);
-  const projectID = requestUrl.searchParams.get("client_id");
+  // const projectID = requestUrl.searchParams.get("client_id");
+  const projectID = "cl82zdzqm0010d4lbgtmm4kc1";
+  // const uid = requestUrl.searchParams.get("uid");
+  const uid = "sdhsadh";
   const t = requestUrl.searchParams.get("t");
   const referer = requestUrl.searchParams.get("ref");
   const width = requestUrl.searchParams.get("w");
   const height = requestUrl.searchParams.get("h");
-
-  console.log(projectID, t, referer, width, height);
+  if (typeof uid === "string" && typeof projectID === "string") {
+    try {
+      const data = await db.user.upsert({
+        where: { userId: uid },
+        update: {},
+        create: {
+          userId: uid,
+          device: device,
+          projectId: projectID,
+          Path: { create: [{ path: "test" }] },
+        },
+      });
+      console.log(data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return null;
 }
